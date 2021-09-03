1079c93d77138d5633b99ffe2f359e2c06edfaea
Thomas Soto <thomassoto94@gmail.com>
2021-09-03 19:03:56 -0400

    agregamos en el app module, el modulo de HttpClientModule para poder realizar el get en el service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
@@ -13,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     SharedModule,
     GifsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  
+  private apiKey: string = "RGICrswqbDNNxdb8SfUmHB4iSxJSLnIS"
   private _historial: string[] = [];
-
 
   //esta funcion retorna los datos que existen en el array
   get historial(){
@@ -15,6 +15,9 @@ export class GifsService {
     return [...this._historial];
   }
 
+  constructor(private http: HttpClient){
+
+  }
 
   //buscamos los gifs y los vamos agregando al array _historia
   buscarGifs(query: string){
@@ -27,9 +30,10 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
     }
 
-    
-
-    console.log(this._historial)
+  this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=RGICrswqbDNNxdb8SfUmHB4iSxJSLnIS&q=dbz&limit=10`)    
+    .subscribe((resp : any) => {
+      console.log(resp.data)
+    })
   }
 
 }
